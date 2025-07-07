bb3799074e2dedc64ece19d14729ba6e7ab70d33
eric406-yeboah <ericusyeboah406@gmail.com>
2025-07-07 20:51:36 +0000

    analogclock

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,190 @@
+import 'dart:math';
+import 'package:flutter/scheduler.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'analog Clock ',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'analog clock'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: AnalogClock(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+  ));
+}
+
+class AnalogClock extends StatefulWidget {
+  @override
+  State<AnalogClock> createState() => _AnalogClockState();
+}
+
+class _AnalogClockState extends State<AnalogClock>
+    with SingleTickerProviderStateMixin {
+  late final Ticker _ticker;
+  DateTime _dateTime = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    // Use Ticker for smooth updates
+    _ticker = Ticker((_) {
+      setState(() {
+        _dateTime = DateTime.now();
+      });
+    })..start();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          width: 300,
+          height: 300,
+          child: CustomPaint(
+            painter: ClockPainter(_dateTime),
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          '${_dateTime.hour.toString().padLeft(2, '0')}:${_dateTime.minute.toString().padLeft(2, '0')}:${_dateTime.second.toString().padLeft(2, '0')}',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+          ),
+        ),
+      ],
     );
   }
+}
+
+class ClockPainter extends CustomPainter {
+  final DateTime dateTime;
+
+  ClockPainter(this.dateTime);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    final fillBrush = Paint()..color = Colors.white;
+
+    final outlineBrush = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 8;
+
+    final centerFillBrush = Paint()..color = Colors.black;
+
+    final shadow = [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.5),
+        offset: Offset(2, 2),
+        blurRadius: 4,
+      )
+    ];
+
+    final secHandBrush = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 2
+      ..strokeCap = StrokeCap.round;
+
+    final minHandBrush = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 4
+      ..strokeCap = StrokeCap.round;
+
+    final hourHandBrush = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 6
+      ..strokeCap = StrokeCap.round;
+
+    // Clock face
+    canvas.drawCircle(center, radius - 10, fillBrush);
+    canvas.drawCircle(center, radius - 10, outlineBrush);
+
+    // Draw numbers
+    for (int i = 1; i <= 12; i++) {
+      double angle = (i * 30) * pi / 180;
+      double x = center.dx + (radius - 30) * cos(angle - pi / 2);
+      double y = center.dy + (radius - 30) * sin(angle - pi / 2);
+      TextPainter textPainter = TextPainter(
+        text: TextSpan(
+            text: '$i',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        textDirection: TextDirection.ltr,
+      )..layout();
+      textPainter.paint(
+        canvas,
+        Offset(x - textPainter.width / 2, y - textPainter.height / 2),
+      );
+    }
+
+    // Draw hour hand
+    double hourDegree =
+        (dateTime.hour % 12 + dateTime.minute / 60) * 30 * pi / 180;
+    final hourHandX = center.dx + 60 * cos(hourDegree - pi / 2);
+    final hourHandY = center.dy + 60 * sin(hourDegree - pi / 2);
+
+    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHandBrush);
+
+    // Draw minute hand
+    double minDegree = (dateTime.minute + dateTime.second / 60) * 6 * pi / 180;
+    final minHandX = center.dx + 80 * cos(minDegree - pi / 2);
+    final minHandY = center.dy + 80 * sin(minDegree - pi / 2);
+
+    canvas.drawLine(center, Offset(minHandX, minHandY), minHandBrush);
+
+    // Draw second hand
+    double secDegree = dateTime.second * 6 * pi / 180;
+    final secHandX = center.dx + 90 * cos(secDegree - pi / 2);
+    final secHandY = center.dy + 90 * sin(secDegree - pi / 2);
+
+    canvas.drawLine(center, Offset(secHandX, secHandY), secHandBrush);
+
+    // Center dot
+    canvas.drawCircle(center, 8, centerFillBrush);
+
+    // Tick marks
+    final tickBrush = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 2;
+
+    final longTickBrush = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 4;
+
+    for (int i = 0; i < 60; i++) {
+      double tickLength = i % 5 == 0 ? 10 : 5;
+      double tickDegree = i * 6 * pi / 180;
+
+      double startX = center.dx + (radius - 10) * cos(tickDegree);
+      double startY = center.dy + (radius - 10) * sin(tickDegree);
+
+      double endX =
+          center.dx + (radius - 10 - tickLength) * cos(tickDegree);
+      double endY =
+          center.dy + (radius - 10 - tickLength) * sin(tickDegree);
+
+      canvas.drawLine(
+        Offset(startX, startY),
+        Offset(endX, endY),
+        i % 5 == 0 ? longTickBrush : tickBrush,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
